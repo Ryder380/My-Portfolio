010b5acc6734f6598df1c5790279482c81e79103
Kalindu <kalindugayanjith@gmail.com>
2025-08-07 14:06:18 +0530

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Index from "./Pages/Home/index";
 import AppLayout from "./Layouts/AppLayout";
+// sd
 import './assets/CSS/App.css'
-import gsap from 'gsap';
-
-
-
-
 function App() {
-
   return (
     <>
       <Routes>
